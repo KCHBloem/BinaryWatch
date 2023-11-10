f53d5f8ccc11042d71314a1d6ad55e4ba62528d4
Unweptpit <71827483+KCHBloem@users.noreply.github.com>
2023-11-10 20:30:06 +0100

    Initial commit

---
+++ Debug/Src/main.d
@@ -0,0 +1,12 @@
+Src/main.o: ../Src/main.c ../Src/../Drivers/Include/stm32l010x4.h \
+ ../Src/../Drivers/Include/core_cm0plus.h \
+ ../Src/../Drivers/Include/cmsis_version.h \
+ ../Src/../Drivers/Include/cmsis_compiler.h \
+ ../Src/../Drivers/Include/cmsis_gcc.h \
+ ../Src/../Drivers/Include/system_stm32l0xx.h
+../Src/../Drivers/Include/stm32l010x4.h:
+../Src/../Drivers/Include/core_cm0plus.h:
+../Src/../Drivers/Include/cmsis_version.h:
+../Src/../Drivers/Include/cmsis_compiler.h:
+../Src/../Drivers/Include/cmsis_gcc.h:
+../Src/../Drivers/Include/system_stm32l0xx.h:
---
+++ Debug/Startup/startup_stm32l010k4tx.d
@@ -0,0 +1,1 @@
+Startup/startup_stm32l010k4tx.o: ../Startup/startup_stm32l010k4tx.s
